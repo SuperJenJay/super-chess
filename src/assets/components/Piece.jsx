a5d8cab3fe2183f9bf0ee7258aab7c51f7357ea0
SuperJenJay <116813028+SuperJenJay@users.noreply.github.com>
2023-04-14 03:20:41 +0200

    Update Piece.jsx

--- src/assets/components/Piece.jsx
+++ src/assets/components/Piece.jsx
@@ -109,7 +109,7 @@ const Piece = (props) => {
     <img
       onClick={onClickHandler}
       className={style}
-      src={`/public/icons/${color}_${figure}.svg`}
+      src="https://svgshare.com/i/s5C.svg"
     />
   );
 };
